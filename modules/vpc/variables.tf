fb42282cdbee269a7927ffbc2563cc06b1f975f7
Samriddhi Shukla <samriddhishukla437@gmail.com>
2026-03-09 19:55:42 +0530

    variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
